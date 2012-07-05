42dedd1696b22d503b162534e356cda87002234d
CiaranR <ciaran.rooney@gmail.com>
2012-07-05 18:02:22 +0100

    Revert "deleting cache files"
    This reverts commit 0877c7583084317b7ad48b4ff791d5c849d2cb31.

---
+++ public_html/cache/8c7775a602923e04c083cfc92a6baa248e9592aa.contact.tpl.php
@@ -0,0 +1,24 @@
+<?php /*%%SmartyHeaderCode:148204ff5c2025f5cd7-31676388%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '8c7775a602923e04c083cfc92a6baa248e9592aa' => 
+    array (
+      0 => '.\\templates\\contact.tpl',
+      1 => 1341505691,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '148204ff5c2025f5cd7-31676388',
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.11',
+  'unifunc' => 'content_4ff5c202606fb2_66254861',
+  'cache_lifetime' => 120,
+),true); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_4ff5c202606fb2_66254861')) {function content_4ff5c202606fb2_66254861($_smarty_tpl) {?>    <div class="container about">
+
+		<h1>Contact Us</h1>
+		<p>&nbsp;</p>
+		<p>You can contact us on <a href="mailto:contact@phplondon.org">contact@phplondon.org</a></p>
+		<p>&nbsp;</p>
+<?php }} ?>
